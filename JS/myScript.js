f7c87f19f2cc4a5c660b9cdc08baae072613ca5d
DoubtMoose <36242412+DoubtMoose@users.noreply.github.com>
2022-01-22 13:46:30 -0600

    Update to frontend. Added JS file

---
+++ JS/myScript.js
@@ -0,0 +1,15 @@
+function changeImg(source)
+{
+    var ticker = document.getElementById("tickerImg");
+    ticker.src = (source);
+}
+
+var ethButton = document.getElementById("ethIcon");
+var btcButton = document.getElementById("btcIcon");
+var dogeButton = document.getElementById("dogeIcon");
+
+// On mouseover events
+
+//ethButton.onmouseover = changeImg("../img/ethChart.png");
+//btcButton.onmouseover = changeImg("../img/ticker.png");
+//dogeButton.onmouseover = changeImg("img/dogeChart.png");
